c4893d7de679010f7153bdc6f3dc8fffb1a9bcd9
Benjamin Tzoc <jtzocb@gmail.com>
2024-03-19 20:26:52 -0600

    Primer commit front traductor

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username: string = "";
+  password: string = "";
+
+  onSubmit() {
+    console.log('Usuario: ' + this.username);
+    console.log('Contraseña: ' + this.password); 
+  }
+}
